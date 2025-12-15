11de848bfb3d44617f22bc08948b02fcc54e973b
Muhammad Ibrahim Shehu <babasmuhammad617@gmail.com>
2025-12-15 02:44:23 +0100

    Switch database configuration to use dj_database_url

--- vulor/settings.py
+++ vulor/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 
 # Build paths
@@ -95,9 +96,10 @@ WSGI_APPLICATION = 'vulor.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
     }
 }
 
