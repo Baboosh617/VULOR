95a7e30a143731517d980888330cbee4dd053845
Baboosh617 <babasmuhammad617@gmail.com>
2026-03-23 22:22:36 +0100

    Blah

--- vulor/urls.py
+++ vulor/urls.py
@@ -17,8 +17,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     # path('django_recaptcha/', include('django_recaptcha.urls')),
     
-    
-    
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
     path('', product_views.home, name='home'),
